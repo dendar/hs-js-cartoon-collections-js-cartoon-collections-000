4f11870e8d00f1d554adc4a1d87ea908ff5c6c2b
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-03-06 17:46:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 function dwarfRollCall(dwarves) {
+  // let str= "";
+  // dwarves.forEach(function(name, index){
+  //   str += (index+1 ". " + name + " ")
+  // });
+  // return str;
+
+  let newStr= [];
+  for(let i=0; i<dwarves.length; i++){
+
+
+
+    newStr.push(((i+1) + ". " +dwarves[i] + " "));
+
+
+   }
+   return newStr;
+  
 }
 
 function summonCaptainPlanet(planeteerCalls){
+
+
+ //  let curA= " "
+ //  for(let i=0;i<planeteerCalls.length;i++){
+ //    let cur =planeteerCalls[i]
+ //
+ //
+ //     curA += `"${cur}!" `
+ //
+ //  }
+ // return curA.toUpperCase()  ;
+
+  //*****************
+  // let str =[];
+  // for(let i=0; i < planeteerCalls.length; i++){
+  //   let curElement= planeteerCalls[i];
+  //   curElement= curElement.toUpperCase()+"!";
+  //   str.push(curElement);
+  // }
+  // return str;
 }
 
 function longPlaneteerCalls(words) {
